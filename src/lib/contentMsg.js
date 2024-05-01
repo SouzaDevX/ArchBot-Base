91d9d6ba91e24ce7c882ee5c34dd0690df16b83a
Aliss0n <88639715+neb6la@users.noreply.github.com>
2024-05-01 16:43:58 -0300

    Update contentMsg.js

--- src/lib/contentMsg.js
+++ src/lib/contentMsg.js
@@ -1,6 +1,6 @@
 /*
-	• Instagram: @ind0minous
-	• Repositório: https://github.com/neb6la/ArchBot-Base
+  • Instagram: @ind0minous
+  • Repositório: https://github.com/neb6la/ArchBot-Base
 */
 
 const { config } = require("../config");
@@ -10,6 +10,7 @@ const regex = urlRegex({ strict: false });
 
 exports.contentMsg = async (client, m) => {
   // Adicionando informações adicionais no objeto de mensagem
+  m.fakeObj = { ...m };
   m.isGroup = m.from.endsWith("@g.us");
   m.sender = m.isGroup ? m.id.participant : m.id.remote;
   m.user = m.sender.split("@")[0];
@@ -29,10 +30,33 @@ exports.contentMsg = async (client, m) => {
   m.contact = await client.getContactById(m.sender);
   m.pushname = m.contact.pushname || m.contact.name || (await client.getFormattedNumber(m.sender));
 
-  m.reply = async (content) => {
-    await client.sendMessage(m.from, content, {
-      quotedMessageId: m.id._serialized,
-    });
+  m.reply = async (content, ops = {}) => {
+    console.log("Enviando mensagem para: ", ops.from || m.from);
+    return await client.sendMess(ops.from || m.from, content, { quoted: m.id._serialized, ...ops });
+  };
+
+  m.getMedia = async (allowedTypes) => {
+    const isTypeAllowed = (type) => allowedTypes.includes(type);
+
+    let sourceMessage = null;
+
+    if (m.hasMedia) {
+      sourceMessage = m;
+    } else if (m.hasQuotedMsg && m.quoted.hasMedia) {
+      sourceMessage = m.quoted;
+    }
+
+    if (!sourceMessage || !isTypeAllowed(sourceMessage.type)) {
+      return false;
+    }
+
+    const media = await sourceMessage.downloadMedia();
+    media.type = sourceMessage.type;
+    media.duration = sourceMessage.duration;
+    media.isMedia = sourceMessage.hasMedia;
+    media.msg = sourceMessage;
+
+    return media;
   };
 
   return m;
